0c16d2d26bd4490e385c7f2f8b29c2e3215acc4a
dev.sandakelum <dev.sandakelum@example.com>
2025-12-26 23:40:20 +0530

    feat: Refactor Home component and implement HomeContent for improved structure and functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,10 @@
-"use client";
-import HeadingNavBar from "@/components/nav/heading-navbar";
-import "./styles/app/main.css";
-import "./styles/app/nav.css";
-import "./styles/app/heading-navbar.css";
-import "./styles/app/scroll-bar.css";
-
-import "./styles/notes/main.css";
-import "./styles/notes/card0.css";
-
-import "./styles/notes-sub/main.css";
-import "./styles/notes-sub/card1.css";
-
-import NavBar from "@/components/nav/main";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import Notes from "@/components/page/notes/notes";
-import { Testdataset } from "@/public/json/dataset";
-import Notes_Sub from "@/components/page/notes/notes-sub";
-import { Fav_dataset } from "@/public/json/fav";
-import { Suggestion_dataset } from "@/public/json/suggest";
+import { Suspense } from "react";
+import HomeContent from "./home-content";
 
 export default function Home() {
-  const params = useSearchParams();
-  const route = params.get("r");
-  const sub = params.get("u");
-  const routeList = Array.from(params.values());
-
-  const [activeIcon, setActiveIcon] = useState<string>(
-    routeList.length === 0 ? "Home" : routeList[0]
-  );
-
-  useEffect(() => {
-    if (routeList.length === 0) {
-      setActiveIcon("Home");
-    } else {
-      setActiveIcon(routeList[0]);
-    }
-  }, [route]);
-
   return (
-    <div
-      className="page root"
-      suppressHydrationWarning
-      onLoad={() => setActiveIcon(routeList[0])}
-    >
-      <NavBar activeIcon={activeIcon} setActiveIcon={setActiveIcon} />
-      <HeadingNavBar data={routeList} />
-      <div className="ContentArea">
-        <div className="UsableArea">
-          {activeIcon === "Notes" && !sub && <Notes datasetA={Fav_dataset} datasetB={Suggestion_dataset} />}
-          {activeIcon === "Notes" && sub && <Notes_Sub dataset={Testdataset} />}
-        </div>
-      </div>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeContent />
+    </Suspense>
   );
 }
