e79fa693f0f8a93b6c2e7f42241aef6c7483133c
dev.sandakelum <dev.sandakelum@example.com>
2025-12-26 17:38:21 +0530

    feat: Implement notes feature with enhanced styling and functionality
    - Added new styles for notes and card components.
    - Introduced a scroll bar customization for better user experience.
    - Integrated a dataset for displaying notes with categories and details.
    - Updated navigation to set default active icon based on search parameters.

--- components/page/notes.tsx
+++ components/page/notes.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import Image from "next/image";
+import { motion } from "framer-motion";
+import Card1 from "./notes/items/card1";
+import { Testdataset } from "@/public/json/dataset";
 
 export default function Notes() {
+  
   return (
-    <div className=''>notes</div>
-  )
+    <motion.div 
+      className="NotesPage"
+      initial="hidden"
+      animate="visible"
+    >
+      {Testdataset.map((category, idx) => (
+        
+        <motion.div 
+          key={category.catogory}
+        >
+          <motion.h1 
+            className="catogory"
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: idx * 0.1 }}
+          >
+            {category.catogory}
+          </motion.h1>
+          <motion.div 
+            className="topic"
+          >
+            {category.data.map((item ,idx2) => (
+              <motion.div
+                key={item.ID}
+                
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: ((idx * category.data.length) + idx2) * 0.1  , duration: 0.4 }}
+              >
+                <Card1 data={item} />
+              </motion.div>
+            ))}
+          </motion.div>
+        </motion.div>
+      ))}
+    </motion.div>
+  );
 }
