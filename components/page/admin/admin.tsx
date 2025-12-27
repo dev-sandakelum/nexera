8f143cfceab8d692a9f10d163ea904d302abf014
dev-sandakelum <dev.sandakelum@gmail.com>
2025-12-27 14:58:33 +0530

    feat: Implement Admin component with dynamic subRoute handling and add styling for MainAdminPage

--- components/page/admin/admin.tsx
+++ components/page/admin/admin.tsx
@@ -1,25 +1,9 @@
-import React from 'react'
+import React from "react";
+import MainAdminPage from "./rotes/main";
 
-export default function Admin() {
-  return (
-    <div>{`n Next.js <Link> (from next/link), you cannot directly add target="_blank" to the <Link> component. Instead, you need to wrap an <a> tag inside <Link> and put the target and rel attributes on the <a>. Like this:
-
-import Link from "next/link";
-import { BsLinkedin } from "react-icons/bs";
-
-<Link href={social.url ? social.url : "#"} key={social.id} passHref>
-  <a target="_blank" rel="noopener noreferrer">
-    <BsLinkedin />
-  </a>
-</Link>
-
-
-✅ Notes:
-
-target="_blank" opens the link in a new tab.
-
-rel="noopener noreferrer" is important for security when using _blank.
-
-passHref ensures the <a> receives the href from <Link>.`}</div>
-  )
+export default function Admin({ subRoute }: { subRoute?: string }) {
+  if (subRoute == "null") {
+    return <MainAdminPage />;
+  }
+  return null
 }
---
+++ components/page/admin/rotes/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function MainAdminPage() {
+  return (
+    <div className="MainAdminPage">
+      <div className="item">1</div>
+      <div className="item">2</div>
+      <div className="item">3</div>
+      <div className="item">4</div>
+      <div className="item">5</div>
+      <div className="item">6</div>
+      <div className="item">7</div>
+      <div className="item">8</div>
+      <div className="item">9</div>
+    </div>
+  );
+}
