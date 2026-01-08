f56710cf4f5154989fc5617be21d9c354ab8581e
dev-sandakelum <dev.sandakelum@gmail.com>
2026-01-08 08:02:51 +0530

    feat: Refactor page component to use LoadingAnimation and enhance mobile styles with smooth scrolling

--- app/(dashboard)/Applications/page.tsx
+++ app/(dashboard)/Applications/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import LoadingAnimation from "@/components/page/loading";
 
 export default function page() {
-  return (
-    <div>page</div>
-  )
+  return <LoadingAnimation />;
 }
